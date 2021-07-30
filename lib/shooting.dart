52a2168f4913b2c366d16efd6518ab5e0a370908
TakabayaP <takabayap@gmail.com>
2021-07-30 15:59:06 +0900

    improve: コメント追加 微修正

--- lib/shooting.dart
+++ lib/shooting.dart
@@ -4,24 +4,26 @@ import 'package:shooting_game/shootingGame.dart';
 
 class Shooting extends StatelessWidget {
   const Shooting({Key? key}) : super(key: key);
+  //なにこれ おまじない？
 
   @override
   Widget build(BuildContext context) {
     ShootingGame game = ShootingGame(onEnd: () {
       Navigator.pop(context);
     });
+    //constructor
     void onEventKey(RawKeyEvent event) {
       game.onKeyEvent(event);
     }
 
+    //shootingGame 内の onKeyEvent をキーイベント時に呼び出す
     return RawKeyboardListener(
+        //リスナーでくるんでキーイベントを発火
         focusNode: FocusNode(),
         onKey: onEventKey,
-        autofocus: true,
+        autofocus: true, //普通にくるむだけだとここがうまく行かない？
         child: GameWidget(
-          game: ShootingGame(onEnd: () {
-            Navigator.pop(context);
-          }),
+          game: game,
         ));
     //GameWidget として渡すことで game を widget として扱える
   }
