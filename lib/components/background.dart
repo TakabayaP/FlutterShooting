6aaaace457a9a16dc3199ec15a55d9df5ef6e12f
TakabayaP <takabayap@gmail.com>
2021-07-30 16:31:35 +0900

    improve: スクロールの向きを変えた

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -1,5 +1,6 @@
 import 'package:flame/components.dart';
 import 'package:flame/parallax.dart';
+import 'package:flutter/widgets.dart';
 import 'package:shooting_game/shootingGame.dart';
 
 class Background extends ParallaxComponent with HasGameRef<ShootingGame> {
@@ -10,7 +11,10 @@ class Background extends ParallaxComponent with HasGameRef<ShootingGame> {
   Future<void> onLoad() async {
     parallax = await gameRef.loadParallax([
       ParallaxImageData(pictureName),
-    ]);
+    ],
+        repeat: ImageRepeat.repeatY,
+        baseVelocity: Vector2(0, 500),
+        fill: LayerFill.width);
   }
   //多分あとでもっと高度な背景クラスを作る(複数枚の背景を重ねて異なる速さで動かすとか)
 }
