d6ebe39ae484570d5a1887f0f0ca61ec0182a043
TakabayaP <takabayap@gmail.com>
2021-08-02 23:24:10 +0900

    improve:余計な行を消した

---
+++ lib/components/enemy.dart
@@ -0,0 +1,15 @@
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+
+class Bullet extends SpriteComponent {
+  late final _image;
+  Bullet({required Sprite sprite, required Vector2 position})
+      : super(sprite: sprite, position: position, size: Vector2(30, 30));
+
+  @override
+  void update(double dt) {
+    this.position += Vector2(0, -1) * 1000 * dt;
+    // TODO: implement update
+    super.update(dt);
+  }
+}
